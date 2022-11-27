68b2cbaf44c8b0fce988408e70bc4f92bac1951e
Miguel Matos <migueamatos02@gmail.com>
2022-11-27 21:13:27 +0000

    commit para a team manager

---
+++ lockedin/src/main/java/ies/grupo51/lockedin/LockedinApplication.java
@@ -0,0 +1,13 @@
+package ies.grupo51.lockedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockedinApplication.class, args);
+	}
+
+}
---
+++ lockedin/src/test/java/ies/grupo51/lockedin/LockedinApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.grupo51.lockedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
