79896e88b206405ec3d06ab064813a9c65737960
Tiago Sora <tiagogcarvalho2002@gmail.com>
2022-11-30 17:37:02 +0000

    go manco

--- lockedin/src/main/java/ies/grupo51/lockedin/services/HealthSensorDataService.java
+++ lockedin/src/main/java/ies/grupo51/lockedin/services/HealthSensorDataService.java
@@ -14,5 +14,31 @@ public class HealthSensorDataService {
     
     @Autowired
     private HealthSensorDataRepository repository;
-    
+
+    public HealthSensorData saveHealthSensorData(HealthSensorData healthSensorData){
+        return repository.save(healthSensorData);
+    }
+
+    public List<HealthSensorData> saveHealthSensorDatas(Set<HealthSensorData> healthSensorDatas) {
+        return repository.saveAll(healthSensorDatas);
+    }
+
+    public List<HealthSensorData> getHealthSensorDatas() {
+        return repository.findAll();
+    }
+
+    public HealthSensorData getHealthSensorDataById(long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public HealthSensorData updateHealthSensorData(HealthSensorData healthSensorData) {
+        HealthSensorData existingHealthSensorData = repository.findById(healthSensorData.getId()).orElse(null);
+        
+        if (existingHealthSensorData == null){ return null; }
+
+        existingHealthSensorData.setBiometrics(healthSensorData.getBiometrics());
+        existingHealthSensorData.setInmate_id(healthSensorData.getInmate_id());
+
+        return repository.save(existingHealthSensorData);
+    }
 }
--- lockedin/src/main/java/ies/grupo51/lockedin/services/MoveSensorDataService.java
+++ lockedin/src/main/java/ies/grupo51/lockedin/services/MoveSensorDataService.java
@@ -15,4 +15,30 @@ public class MoveSensorDataService {
     @Autowired
     private MoveSensorDataRepository repository;
     
+    public MoveSensorData saveMoveSensorData(MoveSensorData moveSensorData){
+        return repository.save(moveSensorData);
+    }
+
+    public List<MoveSensorData> saveMoveSensorData(Set<MoveSensorData> moveSensorDatas) {
+        return repository.saveAll(moveSensorDatas);
+    }
+
+    public List<MoveSensorData> getMoveSensorData() {
+        return repository.findAll();
+    }
+
+    public MoveSensorData getMoveSensorDataById(long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public MoveSensorData updateMoveSensorData(MoveSensorData moveSensorData) {
+        MoveSensorData existingMoveSensorData = repository.findById(moveSensorData.getId()).orElse(null);
+        
+        if (existingMoveSensorData == null){ return null; }
+        
+        existingMoveSensorData.setInmate_id(moveSensorData.getInmate_id());
+        existingMoveSensorData.setNew_area(moveSensorData.getNew_area());
+        
+        return repository.save(existingMoveSensorData);
+    }
 }
