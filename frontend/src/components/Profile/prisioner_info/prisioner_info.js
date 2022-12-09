2b17cc29eb73e95be4941c9ba2c78f9cabd6234d
Pjnp5 <paulojnpinto02@gmail.com>
2022-12-09 21:12:26 +0000

    Profile em peso

---
+++ frontend/src/components/Profile/prisioner_info/prisioner_info.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card, Col, Space } from "antd";
+
+const prisioner_info = () => {
+  return (
+    <Card title="Prisioner Personal Information">
+      <Col
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Space align="start">
+          <Card title="Prisioner Data" style={{ width: "250px" }}>
+            <p>Id: 12</p>
+            <p>Name: PP</p>
+            <p>Birthdate: 12-10-2001</p>
+            <p>Entry Date: 12-10-2021</p>
+            <p>Sentence End: 12-10-2031</p>
+            <p>Solitary: True</p>
+            <p>WorkStationId: 1234567890</p>
+            <p>Health Log Id: 0987654321</p>
+          </Card>
+          <Card title="Health status" style={{ width: "250px" }}>
+            <p>HeartBeat: 70</p>
+            <p>Stress Levels: 15%</p>
+            <p>Glicose Levels: 33</p>
+            <p>Uric Acid: 23</p>
+            <p>Cholestrol: 45</p>
+            <p>Toxic Screen: 12</p>
+          </Card>
+        </Space>
+      </Col>
+    </Card>
+  );
+};
+export default prisioner_info;
--- frontend/src/components/Profile/profile.js
+++ frontend/src/components/Profile/profile.js
@@ -2,6 +2,8 @@ import { Card, Col, Row, Space, Timeline } from "antd";
 import React from "react";
 import { useState } from "react";
 import TheAvatar from "./Avatar/avatar";
+import prisioner_info from "./prisioner_info/prisioner_info";
+import timeline_prisioner from "./timeline/timeline";
 
 const tabList = [
   {
@@ -14,100 +16,10 @@ const tabList = [
   },
 ];
 const contentList = {
-  prisioner_info: (
-    <Col
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Space align="start">
-        <Card title="Data">
-          <p>Id: 12</p>
-          <p>Name: PP</p>
-          <p>Birthdate: 12-10-2001</p>
-          <p>Entry Date: 12-10-2021</p>
-          <p>Sentence End: 12-10-2031</p>
-          <p>Solitary: True</p>
-          <p>WorkStationId: 1234567890</p>
-          <p>Health Log Id: 0987654321</p>
-        </Card>
-        <Card title="Heath status">
-          <p>HeathBeat: 70</p>
-          <p>Stress Levels: 15%</p>
-          <p>Glicose Levels: 33</p>
-          <p>Uric Acid: 23</p>
-          <p>Cholestrol: 45</p>
-          <p>Toxic Screen: 12</p>
-        </Card>
-      </Space>
-    </Col>
-  ),
-  timeline: (
-    <Space>
-      <Col
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Timeline mode="left">
-          <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Solve initial network problems
-          </Timeline.Item>
-          <Timeline.Item>Technical testing</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-        </Timeline>
-      </Col>
-      <Col
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Timeline mode="left">
-          <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Solve initial network problems
-          </Timeline.Item>
-          <Timeline.Item>Technical testing</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-        </Timeline>
-      </Col>
-      <Col
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Timeline mode="left">
-          <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Solve initial network problems
-          </Timeline.Item>
-          <Timeline.Item>Technical testing</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-        </Timeline>
-      </Col>
-    </Space>
-  ),
+  prisioner_info: prisioner_info(),
+  timeline: timeline_prisioner(),
 };
+
 const Profile = () => {
   const [activeTabKey1, setActiveTabKey1] = useState("prisioner_info");
   const onTab1Change = (key) => {
