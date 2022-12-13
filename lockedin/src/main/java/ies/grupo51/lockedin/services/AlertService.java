ea86b577a767857a90abb685821840a5562eae7c
Tiago Sora <tiagogcarvalho2002@gmail.com>
2022-12-13 16:50:54 +0000

    alerts

---
+++ lockedin/src/main/java/ies/grupo51/lockedin/services/AlertService.java
@@ -0,0 +1,34 @@
+package ies.grupo51.lockedin.services;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ies.grupo51.lockedin.exceptions.ResourceNotFoundException;
+import ies.grupo51.lockedin.models.Alert;
+import ies.grupo51.lockedin.repositories.AlertRepository;
+
+@Service
+public class AlertService {
+    
+    @Autowired
+    private AlertRepository repository;
+    
+    public Alert saveAlert(Alert alert){
+        return repository.save(alert);
+    }
+
+    public List<Alert> saveAlerts(Set<Alert> alerts) {
+        return repository.saveAll(alerts);
+    }
+
+    public List<Alert> getAlerts() {
+        return repository.findAll();
+    }
+
+    public Alert getAlertById(long id) throws ResourceNotFoundException {
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource Not Found!"));
+    }
+}
