f06074d04dab4897ee5c09b18f7b186ca21c0e1e
Pjnp5 <paulojnpinto02@gmail.com>
2022-12-15 01:52:22 +0000

    More workstations

--- frontend/src/components/Workstations/workstations.js
+++ frontend/src/components/Workstations/workstations.js
@@ -1,95 +1,210 @@
 import React from "react";
-import { Card, Col } from "antd";
+import { Card, Col, Row } from "antd";
 import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, message, Space } from "antd";
+import { Dropdown, message, Space, Collapse } from "antd";
+import {
+  ScatterChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Scatter,
+} from "recharts";
+const data01 = [
+  {
+    x: 100,
+    y: 200,
+  },
+  {
+    x: 120,
+    y: 100,
+  },
+  {
+    x: 170,
+    y: 300,
+  },
+  {
+    x: 140,
+    y: 250,
+  },
+  {
+    x: 150,
+    y: 400,
+  },
+  {
+    x: 110,
+    y: 280,
+  },
+];
 
-
+const styles = {
+  card: {
+    maxHeight: "100%",
+    backgroundColor: "#D6E4E5",
+  },
+  cardBody: {
+    overflow: "auto",
+    maxHeight: "900px",
+  },
+};
 
 const Workstations = () => (
   <>
     <Col>
       <Card
+        style={styles.card}
+        bodyStyle={styles.cardBody}
         title={
           <div style={{ textAlign: "center", color: "#12494c" }}>
             Workstations
           </div>
         }
-        style={{ backgroundColor: "#D6E4E5" }}
       >
-        <Card
-          type="inner"
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          style={{
-            marginTop: 16,
-          }}
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          style={{
-            marginTop: 16,
-          }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          style={{
-            marginTop: 16,
-          }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          style={{
-            marginTop: 16,
-          }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          style={{
-            marginTop: 16,
-          }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
-        <Card
-          headStyle={{ backgroundColor: "#c2d8d8" }}
-          bodyStyle={{ backgroundColor: "#eff5f5" }}
-          style={{
-            marginTop: 16,
-          }}
-          type="inner"
-          title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
-        >
-          ESTRILHO MESMO COMPLICADO NA CANTINA
-        </Card>
+        <Space align="center" direction="vertical" style={{width: "100%"}}>
+          <Collapse accordion>
+            <Card
+              type="inner"
+              headStyle={{ backgroundColor: "#c2d8d8" }}
+              bodyStyle={{ backgroundColor: "#eff5f5" }}
+              title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
+            >
+              <Row>
+                <Col>Velhice</Col>
+                <Col style={{ alignItems: "end" }}>
+                  <ScatterChart
+                    width={700}
+                    height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="x" name="TimeStamp" />
+                    <YAxis dataKey="y" name="Quota" unit="kg" />
+                    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                    <Legend />
+                    <Scatter
+                      name="Worker Quota per day"
+                      data={data01}
+                      fill="#497174"
+                    />
+                  </ScatterChart>
+                </Col>
+              </Row>
+            </Card>
+            <Card
+              type="inner"
+              headStyle={{ backgroundColor: "#c2d8d8" }}
+              bodyStyle={{ backgroundColor: "#eff5f5" }}
+              title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
+            >
+              <Row>
+                <Col>Velhice</Col>
+                <Col style={{ alignItems: "end" }}>
+                  <ScatterChart
+                    width={700}
+                    height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="x" name="TimeStamp" />
+                    <YAxis dataKey="y" name="Quota" unit="kg" />
+                    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                    <Legend />
+                    <Scatter
+                      name="Worker Quota per day"
+                      data={data01}
+                      fill="#497174"
+                    />
+                  </ScatterChart>
+                </Col>
+              </Row>
+            </Card>
+            <Card
+              type="inner"
+              headStyle={{ backgroundColor: "#c2d8d8" }}
+              bodyStyle={{ backgroundColor: "#eff5f5" }}
+              title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
+            >
+              <Row>
+                <Col>Velhice</Col>
+                <Col style={{ alignItems: "end" }}>
+                  <ScatterChart
+                    width={700}
+                    height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="x" name="TimeStamp" />
+                    <YAxis dataKey="y" name="Quota" unit="kg" />
+                    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                    <Legend />
+                    <Scatter
+                      name="Worker Quota per day"
+                      data={data01}
+                      fill="#497174"
+                    />
+                  </ScatterChart>
+                </Col>
+              </Row>
+            </Card>
+            <Card
+              type="inner"
+              headStyle={{ backgroundColor: "#c2d8d8" }}
+              bodyStyle={{ backgroundColor: "#eff5f5" }}
+              title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
+            >
+              <Row>
+                <Col>Velhice</Col>
+                <Col style={{ alignItems: "end" }}>
+                  <ScatterChart
+                    width={700}
+                    height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="x" name="TimeStamp" />
+                    <YAxis dataKey="y" name="Quota" unit="kg" />
+                    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                    <Legend />
+                    <Scatter
+                      name="Worker Quota per day"
+                      data={data01}
+                      fill="#497174"
+                    />
+                  </ScatterChart>
+                </Col>
+              </Row>
+            </Card>
+            <Card
+              type="inner"
+              headStyle={{ backgroundColor: "#c2d8d8" }}
+              bodyStyle={{ backgroundColor: "#eff5f5" }}
+              title={<div style={{ color: "#12494c" }}>ESTRILHO</div>}
+            >
+              <Row>
+                <Col>Velhice</Col>
+                <Col style={{ alignItems: "end" }}>
+                  <ScatterChart
+                    width={700}
+                    height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="x" name="TimeStamp" />
+                    <YAxis dataKey="y" name="Quota" unit="kg" />
+                    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                    <Legend />
+                    <Scatter
+                      name="Worker Quota per day"
+                      data={data01}
+                      fill="#497174"
+                    />
+                  </ScatterChart>
+                </Col>
+              </Row>
+            </Card>
+          </Collapse>
+        </Space>
       </Card>
     </Col>
   </>
