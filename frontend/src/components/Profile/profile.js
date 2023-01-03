336894c1c192c226764c156a0dd8c4050c88c2f1
mankings <miguelamatos@ua.pt>
2023-01-03 05:24:24 +0000

    where notif

--- frontend/src/components/Profile/profile.js
+++ frontend/src/components/Profile/profile.js
@@ -25,7 +25,9 @@ function Profile() {
 
   const startEditing = () => {
     try {
-      const data = axios.get("http://localhost:5001/api/guard/" + currentUser.id).then((response) => {
+      const role = currentUser.roles.includes("ROLE_ADMIN") ? "warden/profile" : "guard/" + currentUser.id;
+      console.log(role);
+      axios.get("http://localhost:5001/api/" + role).then((response) => {
         console.log(response.data);
         onEditGuard(response.data);
       });
@@ -73,6 +75,78 @@ function Profile() {
             </Button>
           </div>
         </Card>
+        <Modal
+          title="Edit information"
+          open={isEditing}
+          okText="Save"
+          okType="primary"
+          onCancel={() => {
+            ResetEditing();
+          }}
+          onOk={form.submit}>
+          <Space direction="vertical" style={{ width: "100%", height: "100%" }}>
+            <Form
+              form={form}
+              onFinish={(values) => {
+                editGuard(values, editingGuard);
+              }}>
+              <Form.Item name="name" label="Name" initialValue={editingGuard?.name}>
+                <Input
+                  onChange={(e) => {
+                    setEditingGuard((pre) => {
+                      return { ...pre, name: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item name="email" label="Email" initialValue={editingGuard?.email}>
+                <Input
+                  onChange={(e) => {
+                    setEditingGuard((pre) => {
+                      return { ...pre, email: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item name="phone" label="Phone" initialValue={editingGuard?.phone}>
+                <Input
+                  type="number"
+                  onChange={(e) => {
+                    setEditingGuard((pre) => {
+                      return { ...pre, phone: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item name="password" label="Password">
+                <Input.Password
+                  onChange={(e) => {
+                    setEditingGuard((pre) => {
+                      return { ...pre, password: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item
+                name="confirm"
+                label="Confirm password"
+                dependencies={["Password"]}
+                hasFeedback
+                rules={[
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue("password") === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(new Error("The two passwords that you entered do not match!"));
+                    },
+                  }),
+                ]}>
+                <Input.Password />
+              </Form.Item>
+            </Form>
+          </Space>
+        </Modal>
       </div>
     );
   } else if (currentUser.roles.includes("ROLE_USER")) {
@@ -122,29 +196,7 @@ function Profile() {
           onCancel={() => {
             ResetEditing();
           }}
-          onOk={form.submit}
-          // onOk={() => {
-          //   data((guard) => {
-          //     const bool = editGuard(editingGuard);
-          //     if (bool) {
-          //       Modal.success({
-          //         title: "Edit Successful",
-          //         content: `Guard changed with success.`,
-          //         okType: "ghost",
-          //       });
-          //       return editingGuard;
-          //     } else {
-          //       Modal.error({
-          //         title: "Edit Error",
-          //         content: `An error happened while changing the guard.`,
-          //         okType: "danger",
-          //       });
-          //       return guard;
-          //     }
-          //   });
-          //   ResetEditing();
-          // }}
-        >
+          onOk={form.submit}>
           <Space direction="vertical" style={{ width: "100%", height: "100%" }}>
             <Form
               form={form}
@@ -212,75 +264,9 @@ function Profile() {
                     },
                   }),
                 ]}>
-                <Input.Password
-                // onChange={(e) => {
-                //   setEditingGuard((pre) => {
-                //     return { ...pre, password: e.target.value };
-                //   });
-                // }}
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Register
-                </Button>
+                <Input.Password />
               </Form.Item>
             </Form>
-            {/* <Input
-              addonBefore="Name"
-              value={editingGuard?.name}
-              onChange={(e) => {
-                setEditingGuard((pre) => {Edited_guard
-            {/* <Input
-              addonBefore="Email"
-              value={editingGuard?.email}
-              onChange={(e) => {
-                setEditingGuard((pre) => {
-                  return { ...pre, email: e.target.value };
-                });
-              }}
-            /> */}
-            {/* <Input
-              addonBefore="Phone"
-              value={editingGuard?.phone}
-              onChange={(e) => {
-                setEditingGuard((pre) => {
-                  return { ...pre, phone: e.target.value };
-                });
-              }}
-            /> */}
-            {/* <Input
-              addonBefore="AreaId"
-              value={editingGuard?.areaId}
-              onChange={(e) => {
-                setEditingGuard((pre) => {
-                  return { ...pre, areaId: e.target.value };
-                });
-              }}
-            /> */}
-            {/* <Input
-              addonBefore="Old password"
-              value={editingGuard?.password}
-              type="password"
-            />
-            <Input.Password
-              addonBefore="New password"
-              type="password"
-              onChange={(e) => {
-                setEditingGuard((pre) => {
-                  return { ...pre, password: e.target.value };
-                });
-              }}
-            />
-            <Input.Password
-              addonBefore="Confirm new password"
-              type="password"
-              onChange={(e) => {
-                setEditingGuard((pre) => {
-                  return { ...pre, password: e.target.value };Email: pareidreds@aettua.pt
-                });
-              }}
-            /> */}
           </Space>
         </Modal>
       </div>
@@ -288,23 +274,46 @@ function Profile() {
   }
 
   function editGuard(Edited_guard, old_guard) {
-    const new_guard = old_guard;
-    Object.keys(new_guard).map((a) => {
-      if (Edited_guard[a]) new_guard[a] = Edited_guard[a];
-    });
-    try {
-      axios
-        .put("http://localhost:5001/api/guard/" + currentUser.id, new_guard)
-        .catch((error) => {
-          console.log(error);
-        })
-        .then((response) => {
-          console.log(response);
-          return true;
-        });
-    } catch (error) {
-      console.log("Deu pylance");
-      return false;
+    function editGuard(Edited_guard, old_guard) {
+      const new_guard = old_guard;
+      Object.keys(new_guard).map((a) => {
+        if (Edited_guard[a]) new_guard[a] = Edited_guard[a];
+      });
+      try {
+        axios
+          .put("http://localhost:5001/api/guard/" + currentUser.id, new_guard)
+          .catch((error) => {
+            console.log(error);
+          })
+          .then((response) => {
+            console.log(response);
+            return true;
+          });
+      } catch (error) {
+        console.log("Deu pylance");
+        return false;
+      }
+    }
+
+    function editWarden(Edited_warden, old_warden) {
+      const new_warden = old_warden;
+      Object.keys(new_warden).map((a) => {
+        if (Edited_warden[a]) new_warden[a] = Edited_warden[a];
+      });
+      try {
+        axios
+          .put("http://localhost:5001/api/warden/" + currentUser.id, new_warden)
+          .catch((error) => {
+            console.log(error);
+          })
+          .then((response) => {
+            console.log(response);
+            return true;
+          });
+      } catch (error) {
+        console.log("Deu pylance");
+        return false;
+      }
     }
   }
 }
--- lockedin/src/main/java/ies/grupo51/lockedin/controllers/WardenController.java
+++ lockedin/src/main/java/ies/grupo51/lockedin/controllers/WardenController.java
@@ -1,18 +1,25 @@
 package ies.grupo51.lockedin.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
 import ies.grupo51.lockedin.exceptions.ResourceNotFoundException;
 import ies.grupo51.lockedin.models.Warden;
 import ies.grupo51.lockedin.services.WardenService;
 
 
+@Controller
 @CrossOrigin
 @RestController
 @RequestMapping("/api/warden")
@@ -26,5 +33,9 @@ public class WardenController {
         Warden data = wardenService.getWardens().get(0);
         return ResponseEntity.ok().body(data);
     }
-    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Warden> updateWarden(@PathVariable(value = "id") Long id, @Valid @RequestBody Warden wardenDetails) throws ResourceNotFoundException {
+        return ResponseEntity.ok(wardenService.updateWarden(wardenDetails));
+    }
 }
