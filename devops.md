066d5c706eef4fc8d68893b94505a320e03098f2
mankings <miguelamatos@ua.pt>
2023-01-03 06:17:55 +0000

    dale

--- devops.md
+++
@@ -1,9 +0,0 @@
-https://github.com/dsyer/spring-boot-angula
-
-# fuck para o devops
-https://spring.io/guides/tutorials/react-and-spring-data-rest/
-
-# mankini a servir a sauce
-```bash
-docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 -d rabbitmq:3.11-management
-```
---
+++ scripts/update-remote.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+jar_local=./api/ShopAholytics/target/ShopAholytics-0.0.1-SNAPSHOT.jar
+frontend_local=./FrontEnd
+
+jar_remote=~/ies_52/build/api/api.jar
+frontend_remote=~/ies_52/src/client
+
+remote=deti-engsoft-15
+
+echo Username:
+read user
+
+
+printf "[+] Copy jar..."
+scp ${jar_local} ${user}@${remote}:${jar_remote}
+printf "[+] Copy frontend..."
+scp ${frontend_local}/*.html ${frontend_local}/js ${user}@${remote}:${frontend_remote}
+printf "[+] run.sh" 
+ssh ${user}@${remote} "cd ies_52; sudo ./run.sh"
